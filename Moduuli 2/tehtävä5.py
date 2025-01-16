d66ce749204ade7c0fb7d8f3235e9a3049e78eb4
JoonasAir <joonaair@metropolia.fi>
2025-01-16 19:41:51 +0200

    tehtävä5

--- Moduuli 2/tehtävä5.py
+++ Moduuli 2/tehtävä5.py
@@ -1,17 +1,13 @@
-
-
 leiviskat = float(input ("Anna leiviskät: "))
 naulat = float(input ("Anna naulat: "))
 luodit = float(input ("Anna luodit: "))
 
-luoditKg = luodit * 13.3
-naulatKg =
-leiviskatKg =
+LUOTI_G = luodit * 13.3
+NAULA_G = LUOTI_G * 32
+LEIVISKAT_G = NAULA_G * 20
 
+SUMMA_KG = int(LUOTI_G + NAULA_G + LEIVISKAT_G) / 1000
+SUMMA_G = float(LUOTI_G + NAULA_G + LEIVISKAT_G) % 1000
 
-
-tulosKg = (luoditKg + naulatKg + leiviskatKg) / 1000
-
-print(tulosKg)
-
-
+print("Massa nykymittojen mukaan:")
+print(f"{SUMMA_KG} kilogrammaa ja {SUMMA_G:.2f} grammaa.")
