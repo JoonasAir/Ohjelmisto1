887c84b8f2c273a87987695569d05b269ad3d0e8
joonas <joonaair@metropolia.fi>
2025-01-16 12:51:31 +0200

    tehtävä6

---
+++ Moduuli 2/tehtävä5.py
@@ -0,0 +1,17 @@
+
+
+leiviskat = float(input ("Anna leiviskät: "))
+naulat = float(input ("Anna naulat: "))
+luodit = float(input ("Anna luodit: "))
+
+luoditKg = luodit * 13.3
+naulatKg =
+leiviskatKg =
+
+
+
+tulosKg = (luoditKg + naulatKg + leiviskatKg) / 1000
+
+print(tulosKg)
+
+
---
+++ Moduuli 2/tehtävä6.py
@@ -0,0 +1,13 @@
+import random
+
+kolme1 = random.randint(0, 9)
+kolme2 = random.randint(0, 9)
+kolme3 = random.randint(0, 9)
+nelja1 = random.randint(1, 6)
+nelja2 = random.randint(1, 6)
+nelja3 = random.randint(1, 6)
+nelja4 = random.randint(1, 6)
+
+print(f"Numerolukon kolminumeroinen koodi on: {kolme1} {kolme2} {kolme3}")
+print(f"Numerolukon nelinumeroinen koodi on: {nelja1} {nelja2} {nelja3} {nelja4}")
+
