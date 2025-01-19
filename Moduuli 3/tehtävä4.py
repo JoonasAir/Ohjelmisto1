f4fdef1627621db199e61a8aa3f0869b102abfe4
JoonasAir <joonaair@metropolia.fi>
2025-01-19 13:10:43 +0200

    tehtävä4

---
+++ Moduuli 3/tehtävä4.py
@@ -0,0 +1,8 @@
+luku = int(input("Anna vuosiluku: "))
+
+
+
+if ((luku % 4 == 0 and luku % 100 != 0) or luku % 400 == 0):
+    print("Vuosiluku on karkausvuosi")
+else:
+    print("Vuosiluku ei ole karkausvuosi")
