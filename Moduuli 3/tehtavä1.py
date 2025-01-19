00dfdeaf1ab1af8a215ac29837bf0ea3e6c434f8
JoonasAir <joonaair@metropolia.fi>
2025-01-19 13:14:23 +0200

    korjattu

--- Moduuli 3/tehtavä1.py
+++ Moduuli 3/tehtavä1.py
@@ -1,4 +1,4 @@
 pituus = float(input("Anna kuhan pituus senttimetreinä: "))
 
 if pituus <37:
-    print(f"Kala on {35 - pituus} cm alamittainen, laske se takaisin.")
+    print(f"Kala on {37 - pituus:.2f} cm alamittainen, laske se takaisin.")
