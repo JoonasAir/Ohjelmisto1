60cdd87b21d0669a1061a74317b10a20b711d55e
JoonasAir <joonaair@metropolia.fi>
2025-01-21 20:31:02 +0200

    update

---
+++ Moduuli 4/tehtävä2.py
@@ -0,0 +1,6 @@
+tuuma = int(input("Anna tuumat: "))
+
+while tuuma > 0:
+    print(f"{tuuma:} tuuma on {tuuma*2.54} senttimertiä")
+    tuuma = float(input("Anna tuumat: "))
+print("Toiminto lopetettu")
