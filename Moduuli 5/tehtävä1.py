e2effa2a377140d1ca62a7622d02efbb259fac74
JoonasAir <joonaair@metropolia.fi>
2025-01-28 11:37:46 +0200

    done

--- Moduuli 5/tehtävä1.py
+++ Moduuli 5/tehtävä1.py
@@ -2,14 +2,15 @@ import random
 
 maarat = []
 i = 0
-
+summa = 0
 maara = int(input("Anna arpakuutioiden määrä: "))
 
 while i < maara:
     noppa = random.randint(1, 6)
     maarat.append(noppa)
     i += 1
-
    
 for tulos in maarat:
-    print(tulos)    
+    summa = summa + tulos
+
+print(f"Arpakuutioiden silmälukujen summa oli {summa}")    
