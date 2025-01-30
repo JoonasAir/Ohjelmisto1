5a0adb988a4c88ff4ab272f6575c8ade6e6cd24e
joonas <joonaair@metropolia.fi>
2025-01-30 13:30:59 +0200

    update

---
+++ Moduuli 6/tehtävä5.py
@@ -0,0 +1,12 @@
+def luvut(x):
+    parittomat = []
+    for i in x:
+        if i % 2 != 0:
+            parittomat.append(i)
+    return parittomat
+
+kluvut = [1, 2, 3, 4, 5, 6]
+
+l = luvut(kluvut)
+
+print(l)
