da5bc253af9f4f305a4b97e5d0b8c7ed021d7da9
JoonasAir <joonaair@metropolia.fi>
2025-02-01 14:52:39 +0200

    done

---
+++ Moduuli 7/tehtävä1.py
@@ -0,0 +1,16 @@
+kuukaudet = ("Talvi", "Talvi", "Kevät", "Kevät", "Kevät", "Kesä", "Kesä",
+             "Kesä", "Syksy", "Syksy", "Syksy", "Talvi", "Talvi")
+
+
+kknumero = int(input("Syötä kuukauden numero (1-12)"))
+
+vuodenaika = kuukaudet[kknumero-1]
+
+print(f"Syöttämäsi kuukausinumero kuuluu vuodenaikaan: {vuodenaika}")
+
+
+
+
+
+
+
