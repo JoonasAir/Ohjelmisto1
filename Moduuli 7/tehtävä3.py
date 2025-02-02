4a589c4ca37734f72fe52f2649bf22de4ccfe56e
JoonasAir <joonaair@metropolia.fi>
2025-02-02 13:07:19 +0200

    updated

--- Moduuli 7/tehtävä3.py
+++ Moduuli 7/tehtävä3.py
@@ -4,22 +4,41 @@ ICAOkoodit = {"EFAH": "Ahmosuon lentoasema, Oulu",
               "EFKU": "Kuopion lentoasema, Kuopio",
               "EFRY": "Räyskälän lentoasema, Loppi"}
 
-valinta = int(input("Valitse toiminto syöttämällä numero (1-3)\n(1)Syötä uusi lentoasema\n(2)Hae lentoasemaa\n(3)Lopeta ohjelma\nSyötä numero: "))
 
-while valinta != 3:
+
+while True:
+    valinta = (input("Valitse toiminto syöttämällä numero (1-3)\n"
+                        "(1)Syötä uusi lentoasema\n"
+                        "(2)Hae lentoasemaa\n"
+                        "(3)Lopeta ohjelma\n"
+                        "Syötä numero: "))
+    
+    #Jos käyttäjä syöttää merkkijonon, ohjelma tulostaa siitä huomautuksen ja jatkaa while loopin alusta.
+    if not valinta.isdigit():
+        print("Syötä numero!")
+        continue
+
+    #Muutetaan syötä kokonaisluvuksi 
+    valinta = int(valinta)
+
+
     if valinta == 1:
-        uusiICAO = input("Syötä lentoaseman ICAO-koodi: ").strip().upper()
+        uusiICAO = input("Syötä lentoaseman ICAO-koodi: ").strip().upper() #Muutetaan käyttäjän syöttämä merkkijono isoiksi kirjaimiksi
         uusiNimi = input("Syötä lentoaseman nimi: ")
         ICAOkoodit[uusiICAO] = uusiNimi
-        valinta = int(input("Valitse toiminto syöttämällä numero (1-3)\n(1)Syötä uusi lentoasema\n(2)Hae lentoasemaa\n(3)Lopeta ohjelma\nSyötä numero: "))
     elif valinta == 2:
-        haku = input("Syötä lentoaseman ICAO koodi: ").strip().upper()
+        haku = input("Syötä lentoaseman ICAO koodi: ").strip().upper() #Muutetaan käyttäjän syöttämä merkkijono isoiksi kirjaimiksi
         if haku in ICAOkoodit:
             print(ICAOkoodit[haku])
         else:
             print("Lentoasemaa ei löytynyt!")
-        valinta = int(input("Valitse toiminto syöttämällä numero (1-3)\n(1)Syötä uusi lentoasema\n(2)Hae lentoasemaa\n(3)Lopeta ohjelma\nSyötä numero: "))
-    else:
+    elif valinta == 3:
+        print("Ohjelma lopetetaan")
         break
+    else:
+        print("Väärä syöte, kokeile uudestaan!") #Jos käyttäjä syöttää jonkun muun luvun 1-3 ulkopuolelta, ohjelma pyytää syöttämään uuden luvun
 
-print("Ohjelma lopetetaan")
+    
+
+
+
