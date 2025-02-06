2ac282a6f91b9b69a51c5d9d9d845158e2dd8170
joonas <joonaair@metropolia.fi>
2025-02-06 13:56:07 +0200

    updated

--- Moduuli 8/tehtävä2.py
+++ Moduuli 8/tehtävä2.py
@@ -1,7 +1,4 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja tulostaa kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin. 
-#Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä, että pieniä lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
 import mysql.connector
-
 
 def maakoodiHaku(maakoodi):
     sql = f"SELECT type, COUNT(*) FROM airport WHERE iso_country = '{maakoodi}' GROUP BY type"
@@ -11,7 +8,7 @@ def maakoodiHaku(maakoodi):
     tulos = kursori.fetchall()
     if tulos:
         for rivi in tulos:
-            type, maara = rivi #puretaan saatu rivi kahteen listaan
+            type, maara = rivi #puretaan saatu rivi kahteen osaan
             print(type, maara)
     else:
         print("Ei tietoja")
